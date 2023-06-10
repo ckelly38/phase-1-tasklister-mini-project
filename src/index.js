a1aa3221e8c72eb9a0f90f47b5121be6743967ec
Christopher Hugh Kelly <christopher.kelly1997@gmail.com>
2023-06-10 03:27:24 -0600

    task list web page that allows the tasks to be added and then deleted

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  document.getElementById("create-task-form").addEventListener("submit", function(e){
+    e.preventDefault();
+    console.log("submit was clicked!");
+    inptnd = document.getElementById("create-task-form").querySelector("input");
+    //console.log("inptnd = " + inptnd);
+    //console.log(inptnd.value);
+    
+    //create the task element with the content
+    let tsk = "" + inptnd.value;
+    let tskelement = document.createElement("li");
+    tskelement.innerText = tsk;
+
+    //need a button to delete it
+    let delelement = document.createElement("button");
+    delelement.innerText = "done";
+    delelement.addEventListener('click', function(){
+      tskelement.remove();
+    });
+    
+    tskelement.appendChild(delelement);
+    document.getElementById("tasks").appendChild(tskelement);
+  });
 });
